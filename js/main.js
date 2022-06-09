155ecd6bbeff421afba369f5e5a9f690b15d1177
Edoardo22 <vanaliedoardo@gmail.com>
2022-06-09 14:35:20 +0200

    es finito

---
+++ js/main.js
@@ -0,0 +1,15 @@
+const appVue = new Vue({
+    // selettore dell'elemento html dove avviare vue
+    el: "#app",
+
+    // Conterrà tutti i dati necessari all'applicazione
+    data: {
+        h1Title: "ciao mondo",
+        imgPage: "https://picsum.photos/200/300",
+    },
+
+    // Conterrà una serie di funzioni
+    methods: {
+        // funzioni che useremo nell'app
+    }
+});
